95b0ce0cee4935335ea2f6c09a20dd6578667358
Kevin Jahns <kevin.jahns@protonmail.com>
2025-04-05 12:10:15 +0200

    upgrade standard + lint

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,12 @@
-  export default [{
-    input: ['./src/server.js', './src/utils.js', './src/callback.js'],
-    external: id => /^(lib0|yjs|y-protocols|ws|lodash\.debounce|http)/.test(id),
-    output: [{
-      dir: 'dist',
-      format: 'cjs',
-      sourcemap: true,
-      entryFileNames: '[name].cjs',
-      chunkFileNames: '[name]-[hash].cjs'
-    }]
-  }
+export default [{
+  input: ['./src/server.js', './src/utils.js', './src/callback.js'],
+  external: id => /^(lib0|yjs|y-protocols|ws|lodash\.debounce|http)/.test(id),
+  output: [{
+    dir: 'dist',
+    format: 'cjs',
+    sourcemap: true,
+    entryFileNames: '[name].cjs',
+    chunkFileNames: '[name]-[hash].cjs'
+  }]
+}
 ]
